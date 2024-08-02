5633f353db840aa0ea8e0837701b729c91d16164
UPIC\shaun <shaun@u-pic.com>
2024-08-02 14:16:52 -0700

    Initial markup and styles for todo app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,106 @@
-import { useState } from 'react';
-import Another from './Another';
-import logo from '../logo.svg';
+import '../reset.css';
 import '../App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function decrement() {
-    setCount(prevCount => prevCount - 1);
-  }
-
-  function increment() {
-    setCount(prevCount => prevCount + 1);
-  }
-
-  const someStyle = {
-    background: 'blue',
-    color: 'white',
-    fontSize: '28px',
-    fontWeight: 'bold',
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <Another name="Shaun" />
-        <div>
-          <span>{count}</span>
-          <button onClick={decrement}>-</button>
-          <button onClick={increment}>+</button>
+    <div className="todo-app-container">
+      <div className="todo-app">
+        <h2>Todo App</h2>
+        <form action="#">
+          <input
+            type="text"
+            className="todo-input"
+            placeholder="What do you need to do?"
+          />
+        </form>
+
+        <ul className="todo-list">
+          <li className="todo-item-container">
+            <div className="todo-item">
+              <input type="checkbox" />
+              <span className="todo-item-label">Finish Example</span>
+            </div>
+            <button className="x-button">
+              <svg
+                className="x-button-icon"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </li>
+          <li className="todo-item-container">
+            <div className="todo-item">
+              <input type="checkbox" />
+              <span className="todo-item-label">Do something else</span>
+            </div>
+            <button className="x-button">
+              <svg
+                className="x-button-icon"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </li>
+          <li className="todo-item-container">
+            <div className="todo-item">
+              <input type="checkbox" />
+              <span className="todo-item-label">Go to Grocery Example</span>
+            </div>
+            <button className="x-button">
+              <svg
+                className="x-button-icon"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </li>
+        </ul>
+
+        <div className="check-all-container">
+          <div>
+            <div className="button">Check All</div>
+          </div>
+
+          <span>3 items remaining</span>
         </div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {true && <p style={someStyle}>{3 + 2}</p>}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div className="other-buttons-container">
+          <div>
+            <button className="button filter-button filter-button-active">
+              All
+            </button>
+            <button className="button filter-button">Active</button>
+            <button className="button filter-button">Completed</button>
+          </div>
+          <div>
+            <button className="button">Clear completed</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
